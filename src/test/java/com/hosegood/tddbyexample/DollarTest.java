269f4d086eb33ce06a54e52cb31a96aa977dd40f
Chris Hosegood <chris.hosegood@corelogic.co.uk>
2014-02-12 13:10:11 +0000

    CH4 Added test for multiplication

---
+++ src/test/java/com/hosegood/tddbyexample/DollarTest.java
@@ -0,0 +1,19 @@
+package com.hosegood.tddbyexample;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class DollarTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+
+}
