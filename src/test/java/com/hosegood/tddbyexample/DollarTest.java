8accbff58607ecec8a13ddb3c69d51337901644a
Chris Hosegood <chris.hosegood@corelogic.co.uk>
2014-02-12 13:20:16 +0000

    CH3 Added test for equality

--- src/test/java/com/hosegood/tddbyexample/DollarTest.java
+++ src/test/java/com/hosegood/tddbyexample/DollarTest.java
@@ -1,6 +1,7 @@
 package com.hosegood.tddbyexample;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +17,11 @@ public class DollarTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
     }
 
 }
