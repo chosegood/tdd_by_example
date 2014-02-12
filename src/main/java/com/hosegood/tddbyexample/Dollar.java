e2a1d9acd41b4b7468ace6f0080ad8d8cd3689b7
Chris Hosegood <chris.hosegood@corelogic.co.uk>
2014-02-12 13:13:03 +0000

    CH4 Dollar multiplication now passes

---
+++ src/main/java/com/hosegood/tddbyexample/Dollar.java
@@ -0,0 +1,13 @@
+package com.hosegood.tddbyexample;
+
+public class Dollar {
+
+    public int amount = 10;
+
+    public Dollar(int amount) {
+    }
+
+    public void times(int multiplier) {
+    }
+
+}
