a91edc293dc50747afdcef53779ee995bbaa7ca0
Chris Hosegood <chris.hosegood@corelogic.co.uk>
2014-02-12 13:30:42 +0000

    CH5 Added Franc implementation

---
+++ src/main/java/com/hosegood/tddbyexample/Franc.java
@@ -0,0 +1,21 @@
+package com.hosegood.tddbyexample;
+
+public class Franc {
+
+    private final int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Franc franc = (Franc) obj;
+        return this.amount == franc.amount;
+    }
+
+}
