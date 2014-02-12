4417d7c2edeb4194a74f56be5bc3d3ba57811146
Chris Hosegood <chris.hosegood@corelogic.co.uk>
2014-02-12 13:48:59 +0000

    Starting to add factory method to Money

--- src/main/java/com/hosegood/tddbyexample/Money.java
+++ src/main/java/com/hosegood/tddbyexample/Money.java
@@ -3,15 +3,25 @@
  */
 package com.hosegood.tddbyexample;
 
-public class Money {
+public abstract class Money {
 
     protected int amount;
+
+    abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
         return this.amount == money.amount
                 && this.getClass().equals(money.getClass());
+    }
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
     }
 
 }
