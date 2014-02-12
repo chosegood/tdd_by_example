426c72f01d4de1c0db53699c303f76557b88f2e3
Chris Hosegood <chris.hosegood@corelogic.co.uk>
2014-02-12 13:31:41 +0000

    CH6 Renamed DollarTest to MoneyTest

--- src/test/java/com/hosegood/tddbyexample/MoneyTest.java
+++ src/test/java/com/hosegood/tddbyexample/MoneyTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class DollarTest {
+public class MoneyTest {
 
     @Test
     public void testMultiplication() {
