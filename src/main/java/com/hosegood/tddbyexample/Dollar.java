a06d67ff742bc26c4098e2b8b640e99c0b3bb286
Chris Hosegood <chris.hosegood@corelogic.co.uk>
2014-02-12 13:15:26 +0000

    CH4 Refactored to remove hardcoded values

--- src/main/java/com/hosegood/tddbyexample/Dollar.java
+++ src/main/java/com/hosegood/tddbyexample/Dollar.java
@@ -2,12 +2,14 @@ package com.hosegood.tddbyexample;
 
 public class Dollar {
 
-    public int amount = 10;
+    public int amount;
 
     public Dollar(int amount) {
+        this.amount = amount;
     }
 
     public void times(int multiplier) {
+        amount *= multiplier;
     }
 
 }
