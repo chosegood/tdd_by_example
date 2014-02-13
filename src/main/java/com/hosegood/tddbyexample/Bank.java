ce65da16d976b77412f06df277c99195676e496e
Chris Hosegood <chris.hosegood@corelogic.co.uk>
2014-02-13 21:22:57 +0000

    CH14 Introduced the concept of rates

--- src/main/java/com/hosegood/tddbyexample/Bank.java
+++ src/main/java/com/hosegood/tddbyexample/Bank.java
@@ -1,9 +1,48 @@
 package com.hosegood.tddbyexample;
 
+import java.util.Hashtable;
+
 public class Bank {
 
+    private Hashtable<Pair, Integer> rates = new Hashtable<Pair, Integer>();
+
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    int rate(String from, String to) {
+        if (from.equals(to)) {
+            return 1;
+        }
+        Integer rate = rates.get(new Pair(from, to));
+        return rate.intValue();
+    }
+
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), new Integer(rate));
+    }
+
+    private class Pair {
+
+        private String from;
+        private String to;
+
+        Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Pair pair = (Pair) obj;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
+
     }
 
 }
